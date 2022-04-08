ea4af73af3baac609a60666513a800a7b583edfd
Smith Milner <smithmilner@gmail.com>
2022-04-08 00:08:38 -0400

    Adding domain configuration.

---
+++ tf/domain.tf
@@ -0,0 +1,39 @@
+resource "digitalocean_domain" "default" {
+  name = "smithmilner.com"
+}
+
+# Add an A record to the domain for www.example.com.
+resource "digitalocean_record" "prod" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "@"
+  value  = module.prod_cluster.load_balancer_ip
+}
+
+resource "digitalocean_record" "www" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "www"
+  value  = module.prod_cluster.load_balancer_ip
+}
+
+resource "digitalocean_record" "argocd" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "argocd"
+  value  = module.prod_cluster.load_balancer_ip
+}
+
+resource "digitalocean_record" "stage" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "stage"
+  value  = module.stage_cluster.load_balancer_ip
+}
+
+resource "digitalocean_record" "dev" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "dev"
+  value  = module.dev_cluster.load_balancer_ip
+}
